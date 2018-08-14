305845ee6e798ae6ed06089bf3d74818e28c996a
Filip Suchý <nevim42@gmail.com>
2018-08-14 21:41:41 +0200

    Prepared for our structure
    Removed example files and created our simple structure prepared for development. Also ejected React scripts

---
+++ src/components/Application/Application.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+const { Component } = React;
+
+class Application extends Component<{}, {}> {
+
+  public render() {
+    return (
+      <div>
+        <h1>Frontend for Composer CMS</h1>
+        <span>Clean react application ready to develop frontend for Composer CMS.</span>
+      </div>
+    );
+  }
+
+}
+
+export default Application;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+
+import Application from '@source/components/Application';
+import registerServiceWorker from '@source/services/serviceWorker';
+
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <Application />,
   document.getElementById('root') as HTMLElement
 );
+
 registerServiceWorker();
