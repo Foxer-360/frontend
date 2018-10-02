ff28f68dad2ba3cc2498f75722c21df3a1c296e5
Filip Suchý <11656674+nevim42@users.noreply.github.com>
2018-10-02 04:41:46 +0200

    Added plugins and navigations (#9)
    * Added plugin navigations

--- src/services/modules/plugins.tsx
+++ src/services/modules/plugins.tsx
@@ -1,9 +1,96 @@
-//  import { config, ILibDefinition, plugins } from './config';
+import * as React from 'react';
+import { config, LibDefinition, plugins } from './config';
+
+// tslint:disable:max-classes-per-file
+
+interface INamedInstances {
+  [name: string]: any; // tslint:disable-line:no-any
+}
+
+interface IName2InstanceMap {
+  [name: string]: string;
+}
+
+class NotFound extends React.Component<{}, {}> {
+
+  public render() {
+    return (
+      <div>
+        <span>Component not found!</span>
+      </div>
+    );
+  }
+
+}
 
 class PluginsModule {
 
+  private names: string[];
+
+  private instances: INamedInstances;
+
+  private name2instance: IName2InstanceMap;
+
   constructor() {
-    // To Do
+    this.names = [] as string[];
+    this.instances = {};
+    this.name2instance = {};
+
+    const plgs = config.plugins;
+    plgs.forEach((lib: LibDefinition) => {
+      const inst = new plugins[lib.name]();
+      this.instances[lib.name] = inst;
+
+      const types = inst.getPluginTypes();
+      types.forEach((type: string) => {
+        this.name2instance[type] = lib.name;
+      });
+      this.names = [
+        ...this.names,
+        ...types,
+      ];
+    });
+  }
+
+  public getPluginTypes(): string[] {
+    return this.names;
+  }
+
+  public getPluginComponent(type: string): typeof React.Component {
+    const name = this.name2instance[type];
+    const i = this.instances[name];
+
+    if (!name || !i) {
+      return this.getNotFoundComponent();
+    }
+
+    return i.getPluginComponent(type);
+  }
+
+  public getPlugin(type: string) {
+    const name = this.name2instance[type];
+    const i = this.instances[name];
+
+    if (!name || !i) {
+      return null;
+    }
+
+    return i.getPlugin(type);
+  }
+
+  public getPluginTabName(type: string): string {
+    const name = this.name2instance[type];
+    const i = this.instances[name];
+
+    if (!name || !i) {
+      return `Undefined ${type}`;
+    }
+
+    return i.getPluginTabName(type);
+  }
+
+  private getNotFoundComponent(): typeof React.Component {
+    return NotFound;
   }
 
 }
