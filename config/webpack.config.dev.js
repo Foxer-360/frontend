066ad20683eee547e130982e69c08f2c90dc6c96
Filip Suchý <nevim42@gmail.com>
2019-05-13 15:22:42 +0200

    Enabled grid into autoprefixer

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -209,6 +209,7 @@ module.exports = {
                         'not ie < 9', // React doesn't support IE8 anyway
                       ],
                       flexbox: 'no-2009',
+                      grid: true,
                     }),
                   ],
                 },
@@ -245,6 +246,7 @@ module.exports = {
                         'not ie < 9', // React doesn't support IE8 anyway
                       ],
                       flexbox: 'no-2009',
+                      grid: true,
                     }),
                   ],
                 },
